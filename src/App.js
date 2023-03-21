2b5fb586642294f2ae07f9b8c6145fe6c3512ac1
Mallory Shackford <93612680+MalloryShackford@users.noreply.github.com>
2023-03-21 12:23:12 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Table from './components/Table'
+
+const App = () => {
+  return (
+    <div className="departures">
+      <header>DEPARTURES</header>
+      <Table/>
+    </div>
+  )
+}
+
+export default App
+
+/*{
+  "clientID": "PELMxuoGNMgDDFUtArfoPKMr"
+  "clientSecret": "Q6qDRZQWTe.O2NzF84pLR,QSSigSYG74GZ+7rN6inXzvt9_UGKfFWl0ag5RfWZ45vSE6oEOyYB.xYP9MFnxNz+_YtFP6xA7w8PPMPQXJ-6+4eHjdYjZ9RNEZ4Hp.h+oE"
+  "token": "AstraCS:PELMxuoGNMgDDFUtArfoPKMr:b551f4cb31e3999163a2bc2f0724889c9902d1a499b413a5ba577f0f2f38edde"
+}*/
---
+++ src/components/TableRow.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import TableCell from './TableCell'
+
+const TableRow = ({flight}) => {
+ const words = Object.values(flight)
+
+  return (
+    <tr>
+      <td>✈︎</td>
+      {words?.map((word, _index) => (
+      <TableCell key={_index} word={word} />
+      ))}
+  
+    </tr>
+  )
+}
+
+export default TableRow
